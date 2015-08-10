78e002a0d0554f5fd436b73ec3ba7b91f7e91103
duan <1418929937@qq.com>
2015-08-10 16:56:18 +0800

    HomeActivity界面

--- src/com/capacity/smartglass/HomeActivity.java
+++ src/com/capacity/smartglass/HomeActivity.java
@@ -11,5 +11,9 @@ import android.widget.TextView;
 
 public class HomeActivity extends Activity {
 	
-	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
 }
--- src/com/capacity/smartglass/Setup3Activity.java
+++ src/com/capacity/smartglass/Setup3Activity.java
@@ -3,6 +3,7 @@ package com.capacity.smartglass;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Setup3Activity extends BaseSetupActivity {
 	
@@ -12,6 +13,10 @@ public class Setup3Activity extends BaseSetupActivity {
 		setContentView(R.layout.activity_setup3);
 	}
 
+	public void next(View view) {
+		showNext();
+	}
+	
 	@Override
 	public void showPref() {
 		Intent i = new Intent(Setup3Activity.this, Setup2Activity.class);
@@ -22,6 +27,9 @@ public class Setup3Activity extends BaseSetupActivity {
 
 	@Override
 	public void showNext() {
-		
+		Intent i = new Intent(Setup3Activity.this, HomeActivity.class);
+		startActivity(i);
+		finish();
+		overridePendingTransition(R.anim.page_next, R.anim.page_exit);
 	}
 }
